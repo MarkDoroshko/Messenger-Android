41f23b9d7cc2e75acd943a1968bdbb6594cc29fa
Mark <markdoroshko@yandex.ru>
2026-02-16 14:08:12 +0300

    Добавление Retrofit и kotlinx.serialization зависимостей в проект.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,8 @@ android {
 }
 
 dependencies {
+    implementation(libs.converter.kotlinx.serialization)
+    implementation(libs.retrofit)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
